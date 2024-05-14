a3b99c198639751993414f01acc27488e561a1db
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 08:07:01 +0700

    Add InvoiceService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootinvoice/service/InvoiceService.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.springbootinvoice.service;
+
+import id.my.hendisantika.springbootinvoice.repository.InvoiceRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/14/24
+ * Time: 08:06
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class InvoiceService {
+    private final InvoiceRepository invoiceRepository;
+}
