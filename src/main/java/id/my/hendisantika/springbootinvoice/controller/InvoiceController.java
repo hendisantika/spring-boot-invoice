7e86b2d8e5c7a0002f59624d2af321ee743f00b6
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 08:09:00 +0700

    Add InvoiceController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootinvoice/controller/InvoiceController.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.springbootinvoice.controller;
+
+import id.my.hendisantika.springbootinvoice.service.InvoiceService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/14/24
+ * Time: 08:08
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Controller
+@RequestMapping("/invoice")
+@RequiredArgsConstructor
+public class InvoiceController {
+    private final InvoiceService invoiceService;
+}
