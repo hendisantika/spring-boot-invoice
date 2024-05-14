10a859d6654381e61a6f93e6c3b98a82b6b5bcca
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 08:06:01 +0700

    Add InvoiceNotFoundException🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootinvoice/exception/InvoiceNotFoundException.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.springbootinvoice.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/14/24
+ * Time: 08:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class InvoiceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvoiceNotFoundException() {
+        super();
+    }
+
+    public InvoiceNotFoundException(String customMessage) {
+        super(customMessage);
+    }
+}
