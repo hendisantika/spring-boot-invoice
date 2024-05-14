166f088c181e65b86e8750a6bcd6782f9606fa78
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 08:03:05 +0700

    Create spring boot project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootinvoice/SpringBootInvoiceApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootinvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInvoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInvoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootinvoice/SpringBootInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootinvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInvoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
